eb67e1ebd3cff1305980bf9b104acac1dd38eddd
Jiayi0928 <mmajiayi@163.com>
2024-02-17 18:40:54 +0800

    hide url

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import UserController from "./controllers/users/users-controllers.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import "./config.js";
 
-const CONNECTION_STRING = "mongodb+srv://fullstack:123@cluster0.psq34.mongodb.net/?retryWrites=true&w=majority";
+// const CONNECTION_STRING = "mongodb+srv://fullstack:123@cluster0.psq34.mongodb.net/?retryWrites=true&w=majority";
 // mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
 mongoose.connect(process.env.MONGODB_URI);
 
